343af7610d5e6b3ccde3b1a0fd1c46c84636c220
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-09 16:10:10 +0300

    Create enum Group

---
+++ src/main/java/ua/alexkras/Group.java
@@ -0,0 +1,12 @@
+package ua.alexkras;
+
+public enum Group {
+
+    group1("Group1"),
+    group2a("Group2a"),
+    group2b("Group2b");
+    String name;
+    Group(String name){
+        this.name = name;
+    }
+}
